57d147020389fd65ae9aef1324e8180b8b0ffc73
PrashantBtkl <prashant44backup@gmail.com>
2025-02-08 14:43:24 +0530

    add: on change script

---
+++ run_onchange_reload-config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Reload i3 configuration
+if pgrep i3 > /dev/null; then
+    i3-msg reload
+fi
+
+# Reload kitty configuration
+if pgrep kitty > /dev/null; then
+    pkill -USR1 kitty
+fi
+
+# Source zshrc in all active zsh sessions
+for pid in $(pgrep zsh); do
+    if [[ -d "/proc/$pid" ]]; then
+        echo "source ~/.zshrc" > /proc/$pid/fd/0
+    fi
+done
